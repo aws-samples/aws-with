425e425726e5de1dd63abe5c6945ec9aa524d8b7
miev <miev@amazon.com>
2017-12-14 07:33:11 +0000

    changes to support new development pipeline

--- aws_with/__init__.py
+++ aws_with/__init__.py
@@ -9,7 +9,7 @@
 #from . import cli
 #from . import main
 
-VERSION = "0.9.7"
+VERSION = "0.9.8"
 BUILD_TIME = "2017-12-12 08:16:36"
 BUILD_COMMIT_HASH = "f2ddf932435f6426a8758c2a23e511e4e1df5e34"
 
@@ -20,4 +20,3 @@ __license__ = "APACHE2"
 __uri__ = "https://aws.amazon.com/"
 __author__ = "Mike Evans"
 __email__ = "miev@amazon.com"
-
--- run-test-pipeline.sh
+++ run-test-pipeline.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# read test pipeline configuration details...
+# read pipeline configuration options
 source pipeline.cfg
-
-# copy source files to a temporary directory...
-ZIPDIR=`mktemp -d`
-cp -R aws_with/ tests/ setup.py setup.cfg README.rst *.yml $ZIPDIR
 
 # zip it up...
 ZIP=`mktemp -u --suffix=.zip`
-(
-    cd $ZIPDIR
-    zip -r $ZIP .
-)
+git ls-tree -r --name-only master | zip -@ $ZIP
 
 # copy the zip to S3...
 aws s3 cp $ZIP $TEST_S3_PATH
